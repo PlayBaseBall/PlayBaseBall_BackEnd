2b147dee3bd79e3276affe01bf33081661d97917
KimGeonWoo <ikkw2040@naver.com>
2024-10-09 14:22:34 +0900

    constants url

---
+++ pb.view/src/main/java/doubleplay/playbaseball/constants/Constants.java
@@ -0,0 +1,13 @@
+package doubleplay.playbaseball.constants;
+
+public class Constants {
+    public static class URI {
+
+    }
+    public static class ADMIN_URI {
+
+    }
+    public static class USER_URI {
+
+    }
+}
