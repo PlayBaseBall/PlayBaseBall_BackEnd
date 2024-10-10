7ef9799192fb88e858529fa1596a59e56df144cd
KimGeonWoo <ikkw2040@naver.com>
2024-10-10 23:11:34 +0900

    @SpringBootApplication 적용

--- pb.view/src/main/java/doubleplay/playbaseball/PlayBaseBallApplication.java
+++ pb.view/src/main/java/doubleplay/playbaseball/PlayBaseBallApplication.java
@@ -1,6 +1,9 @@
 package doubleplay.playbaseball;
 
-public class Main {
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class PlayBaseBallApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
