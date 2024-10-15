536ee31f8e9407512f663bf7924568ce81931b96
KimGeonWoo <ikkw2040@naver.com>
2024-10-15 23:23:15 +0900

    team 엔티티 컬럼 추가

--- pb.core/src/main/java/doubleplay/playbaseball/entities/team/Team.java
+++ pb.core/src/main/java/doubleplay/playbaseball/entities/team/Team.java
@@ -17,4 +17,24 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "TEAM_ID", nullable = false, length = 15)
     private Long team_id;
+
+    @Basic
+    @Comment("경기")
+    @Column(name = "GAME")
+    private Integer game;
+
+    @Basic
+    @Comment("승")
+    @Column(name = "WIN")
+    private Integer win;
+
+    @Basic
+    @Comment("경기")
+    @Column(name = "LOSS")
+    private Integer loss;
+
+    @Basic
+    @Comment("무승부")
+    @Column(name = "DRAW")
+    private Integer draw;
 }
