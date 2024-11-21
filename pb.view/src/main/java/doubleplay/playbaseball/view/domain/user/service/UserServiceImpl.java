10fad887893fa2f879d3f9e161df1dd4ebe8bf3c
kimgeonwoo <ikkw2040@naver.com>
2024-11-21 19:27:08 +0900

    [UPDATE] 로그인시 유효성 검증 코드 추가

--- pb.view/src/main/java/doubleplay/playbaseball/view/domain/user/service/UserServiceImpl.java
+++ pb.view/src/main/java/doubleplay/playbaseball/view/domain/user/service/UserServiceImpl.java
@@ -19,13 +19,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public ApiResult<?> join(UserDTO.JOIN dto) {
-
-        if (isUsernameTaken(dto.getUsername())) {
-            throw new IllegalArgumentException("Username is already taken.");
-        }
-        if (isEmailTaken(dto.getEmail())) {
-            throw new IllegalArgumentException("Email is already registered.");
-        }
+        validateJoinRequest(dto);
 
         User user = new User();
         user.setUsername(dto.getUsername());
@@ -36,6 +30,28 @@ public class UserServiceImpl implements UserService{
                 .result(ApiResultCode.SUCCESS)
                 .message(SystemCode.COMMON_MESSAGE.SUCCESS.getLabel())
                 .build();
+    }
+
+
+    /**
+     * 회원가입 요청 데이터 유효성 검증
+     */
+    private void validateJoinRequest(UserDTO.JOIN dto) {
+        if (dto.getUsername() == null || dto.getUsername().isEmpty()) {
+            throw new IllegalArgumentException("아이디를 쓰세요.");
+        }
+        if (dto.getEmail() == null || dto.getEmail().isEmpty()) {
+            throw new IllegalArgumentException("이메일을 쓰세요.");
+        }
+        if (dto.getPassword() == null || dto.getPassword().length() < 6) {
+            throw new IllegalArgumentException("비밀번호는 적어도 6자 이상이어야 합니다.");
+        }
+        if (isUsernameTaken(dto.getUsername())) {
+            throw new IllegalArgumentException("이미 등록된 아이디입니다.");
+        }
+        if (isEmailTaken(dto.getEmail())) {
+            throw new IllegalArgumentException("이미 등록된 이메일입니다.");
+        }
     }
 
     public boolean isUsernameTaken(String username) {
